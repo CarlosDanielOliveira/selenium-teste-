63473b39965cdba17b9b4d3c77d5687ed51f4f21
Carlos Daniel Oliveira <carlosdanielbarbosadeoliveira@gmail.com>
2025-03-08 16:06:08 -0300

    find_linktext

---
+++ find_linktest.py
@@ -0,0 +1,58 @@
+# selenium 4
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+
+def iniciar_driver():
+    chrome_options = Options()
+    # Fonte de opções de switches https://peter.sh/experiments/chromium-command-line-switches/
+
+    arguments = ['--lang=pt-BR', '--window-size=1000, 900',
+                '--incognito', '--headless' ]
+                
+    
+    ''' Common arguments
+    --start-maximized # Inicia maximizado
+    --lang=pt-BR # Define o idioma de inicialização, # en-us , pt-BR
+    --incognito # Usar o modo anônimo
+    --window-size=800,800 # Define a resolução da janela em largura e altura
+    --headless # Roda em segundo plano(com a janela fechada)
+    --disable-notifications # Desabilita notificações
+    --disable-gpu # Desabilita renderização com GPU
+    '''
+    for argument in arguments:
+        chrome_options.add_argument(argument)
+
+    caminho_padrao_para_download = 'C:\\Users\\carlo\\Desktop\\CURSO automação com python\\download'
+
+    # Lista de opções experimentais(nem todas estão documentadas) https://chromium.googlesource.com/chromium/src/+/32352ad08ee673a4d43e8593ce988b224f6482d3/chrome/common/pref_names.cc
+    chrome_options.add_experimental_option("prefs", {
+        'download.default_directory': caminho_padrao_para_download,
+        # Atualiza diretório para diretório passado acima
+        'download.directory_upgrade': True,
+        # Setar se o navegar deve pedir ou não para fazer download
+        'download.prompt_for_download': False,
+        "profile.default_content_setting_values.notifications": 2,  # Desabilita notificações
+        # Permite realizar múltiplos downlaods multiple downloads
+        "profile.default_content_setting_values.automatic_downloads": 1,
+    })
+
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
+
+driver = iniciar_driver()
+driver.get('https://cursoautomacao.netlify.app/')
+
+#Usado com HTML <a>login</a>
+link_home_navegacao = driver.find_element(By.LINK_TEXT, 'Home')  #Poderia buscarr por parte parcial do texto (By.PARTIAL_LINK_TEXT, 'Ho')
+
+if link_home_navegacao is not None:
+    print('Foi encontrado - link HOME')
+
+
+input('')
+driver.close()
+
+
+
+
